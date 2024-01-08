c3d0e04bca402902baa26eda3ac5a6ff1b5f3276
robertpohl <robban.p@gmail.com>
2024-01-08 22:18:04 +0100

    tests

--- src/helpers.rs
+++ src/helpers.rs
@@ -16,3 +16,21 @@ pub fn run_command(cmd: &str, args: Vec<&str>) -> anyhow::Result<String> {
         String::from_utf8(command.execute_output()?.stdout)?
     )
 }
+
+#[cfg(test)] 
+mod test {
+    use crate::helpers::shorten;
+    use super::run_command;
+    
+    #[test]
+    fn test_shorten() {
+        let truth = "hell";
+        assert_eq!(shorten("hello", 4),truth)
+    }
+
+    #[test]
+    fn test_run_command() {
+        let output = run_command("ls", vec!["-la"]);
+        assert!(output.is_ok())
+    }
+}
